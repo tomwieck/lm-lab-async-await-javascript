54d870518f2096933bc77dcfabc68b4c09c9750f
Tom <admin@Admins-MacBook-Pro.local>
2023-01-31 12:01:21 +0000

    Convert to async await

--- exercise001.js
+++ exercise001.js
@@ -1,19 +1,25 @@
-const promise = new Promise((resolve, reject) => {
-	// Generate random number between 0 and 9
-	const randomInt = Math.floor(Math.random() * 10);
+async function getPromiseResponse() {
+	const promise = new Promise((resolve, reject) => {
+		// Generate random number between 0 and 9
+		const randomInt = Math.floor(Math.random() * 10);
+	
+		if (randomInt % 2 === 0) {
+			// Success
+			setTimeout(() => resolve('done'), 2000);
+		} else {
+			// Failure
+			setTimeout(() => reject('error'), 2000);
+		}
+	});
+	let result = await promise; // wait until the promise resolves (*)
 
-	if (randomInt % 2 === 0) {
-		// Success
-		setTimeout(() => resolve('done'), 2000);
-	} else {
-		// Failure
-		setTimeout(() => reject('error'), 2000);
+	if (result === 'done') {
+		console.log(`Yay! Promise resolved with response: ${result}`);
 	}
-});
+}
 
-promise.then((response) => {
-	console.log(`Yay! Promise resolved with response: ${response}`)
-})
-.catch((error) => {
-	console.log(`Boo. Promise rejected with response: ${error}`)
-})
+getPromiseResponse()
+	.catch(e => { 
+		console.log(`Boo. Promise rejected with response: ${e}`) 
+	});
+
